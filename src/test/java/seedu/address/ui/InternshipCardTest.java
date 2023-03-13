29f75dc325b7a47c0dbf76c0d2132d7634df56be
eugenetangkj <dragoneugene10@gmail.com>
2023-03-13 15:54:59 +0800

    Separate imports

--- src/test/java/seedu/address/ui/InternshipCardTest.java
+++ src/test/java/seedu/address/ui/InternshipCardTest.java
@@ -3,7 +3,6 @@ package seedu.address.ui;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 import org.junit.jupiter.api.Test;
 
 import seedu.address.testutil.TypicalInternships;
