0798391a38a05df78332d8af4c9baaf2e98b02f2
eugenetangkj <dragoneugene10@gmail.com>
2023-03-13 15:49:14 +0800

    Update

--- src/test/java/seedu/address/ui/InternshipCardTest.java
+++ src/test/java/seedu/address/ui/InternshipCardTest.java
@@ -3,14 +3,14 @@ package seedu.address.ui;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+//import java.util.ArrayList;
+//import java.util.HashMap;
 
 import org.junit.jupiter.api.Test;
 
-import javafx.scene.paint.Color;
-import seedu.address.model.internship.Internship;
-import seedu.address.testutil.InternshipBuilder;
+//import javafx.scene.paint.Color;
+//import seedu.address.model.internship.Internship;
+//import seedu.address.testutil.InternshipBuilder;
 import seedu.address.testutil.TypicalInternships;
 
 public class InternshipCardTest extends GuiUnitTest {
@@ -25,23 +25,23 @@ public class InternshipCardTest extends GuiUnitTest {
 //    }
 
 
-//    @Test
-//    public void equals() {
-//        InternshipCard internshipCardAmazonOne = new InternshipCard(TypicalInternships.AMAZON, 1);
-//        InternshipCard internshipCardAmazonTwo = new InternshipCard(TypicalInternships.AMAZON, 2);
-//        InternshipCard internshipCardAmazonOneDuplicate = new InternshipCard(TypicalInternships.AMAZON, 1);
-//        //Same object
-//        assertTrue(internshipCardAmazonOne.equals(internshipCardAmazonOne));
-//
-//        //Not an instance of internship card
-//        assertFalse(internshipCardAmazonOne.equals(TypicalInternships.AMAZON));
-//
-//        //Same internship but different index should not be equal
-//        assertFalse(internshipCardAmazonOne.equals(internshipCardAmazonTwo));
-//
-//        //Different objects but same internship and index
-//        assertTrue(internshipCardAmazonOne.equals(internshipCardAmazonOneDuplicate));
-//    }
+    @Test
+    public void equals() {
+        InternshipCard internshipCardAmazonOne = new InternshipCard(TypicalInternships.AMAZON, 1);
+        InternshipCard internshipCardAmazonTwo = new InternshipCard(TypicalInternships.AMAZON, 2);
+        InternshipCard internshipCardAmazonOneDuplicate = new InternshipCard(TypicalInternships.AMAZON, 1);
+        //Same object
+        assertTrue(internshipCardAmazonOne.equals(internshipCardAmazonOne));
+
+        //Not an instance of internship card
+        assertFalse(internshipCardAmazonOne.equals(TypicalInternships.AMAZON));
+
+        //Same internship but different index should not be equal
+        assertFalse(internshipCardAmazonOne.equals(internshipCardAmazonTwo));
+
+        //Different objects but same internship and index
+        assertTrue(internshipCardAmazonOne.equals(internshipCardAmazonOneDuplicate));
+    }
 //
 //    @Test
 //    public void create_colorMap_success() {
